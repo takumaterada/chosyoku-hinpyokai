bb2fc1cffabaa5b0e89899a760d165e02fbfafd1
takumaterada <nsf3021@gmail.com>
2022-04-18 10:03:39 +0000

    blongs_to 修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,3 @@
 class RelationshipsController < ApplicationController
 
-  belongs_to :follower, class_name: "User"
-  belongs_to :followed, class_name: "User"
-
 end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,7 +1,7 @@
 class Relationship < ApplicationRecord
 
-  belongs_to :followed, class_name: "User"
   belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
 
       # フォローするとき
   def create
